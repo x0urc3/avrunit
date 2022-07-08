2978323a424d473945f29bc2a4fb70c0b8e416e2
x0urc3 <xource@gmail.com>
2022-07-08 11:45:10 +0000

    Declare data structure to store test statistics

--- src/avrunit.h
+++ src/avrunit.h
@@ -1,3 +1,6 @@
+#ifndef _AVRUNIT_H
+#define _AVRUNIT_H
+
 #include <avr/io.h>
 #include <avr/eeprom.h>
 
@@ -6,22 +9,6 @@
 #define MU_BROKEN 2
 #define MU_IGNORE 3
 
-typedef uint8_t ut8;
-typedef uint16_t size_t;
-
-struct eeDataStruct {
-    uint16_t run;
-    uint16_t fail;
-    uint16_t broken;
-    uint16_t ignore;
-    uint16_t failID[10];
-    uint16_t brokenID[10];
-    uint16_t ignoreID[10];
-};
-
-struct eeDataStruct EEMEM mu_ee_Stat = {0};     // Required to be defined
-                                                //
-//struct muDataStruct {
 typedef struct {
     uint16_t run;
     uint16_t fail;
@@ -30,10 +17,20 @@ typedef struct {
     uint16_t failID[10];
     uint16_t brokenID[10];
     uint16_t ignoreID[10];
-} muDataStruct ;
+} stat_t;
 
-//struct muDataStruct mu_stat;
-struct muDataStruct mu_stat;
+/*
+#define mu_setup do { \
+    stat_t EEMEM mu_ee_stat = {0}; \
+    stat_t mu_stat = {0}; \
+    int mu_tests_run = 0; \
+    int mu_tests_fail = 0; \
+    int mu_tests_broken = 0; \
+    int mu_tests_ignore = 0; \
+} while (0)
+*/
+stat_t EEMEM mu_ee_stat = {0};
+stat_t mu_stat = {0};
 
 #define mu_assert(test) do { if (!(test)) { return MU_FAIL; } } while (0)
 #define mu_broken(test) do { return MU_BROKEN; } while (0)
@@ -44,24 +41,21 @@ struct muDataStruct mu_stat;
 #define mu_run_test_named(test, ...) do { \
     int result; \
     result = test(__VA_ARGS__); \
-    mu_tests_run++; \
+    mu_stat.run++; \
     switch (result) { \
         case MU_FAIL: \
             mu_stat.fail += 1; \
             break; \
         case MU_BROKEN: \
-            mu_tests_broken += 1; \
+            mu_stat.broken += 1; \
             break; \
         case MU_IGNORE: \
-            mu_tests_ignore += 1; \
+            mu_stat.ignore += 1; \
             break; \
-    }\
+    } \
 } while (0)
 
 #define mu_run_test(test, ...) mu_run_test_named (test, __VA_ARGS__)
 
-mu_stat.fail = 0;
-int mu_tests_run = 0;
-int mu_tests_fail = 0;
-int mu_tests_broken = 0;
-int mu_tests_ignore = 0;
+
+#endif // _AVRUNIT_H
