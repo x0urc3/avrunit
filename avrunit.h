06f9ffb1f34611524a4aec942322679c9db70799
Khairulmizam Samsudin <xource@gmail.com>
2022-07-26 02:04:08 +0000

    Change stat_t and stat_fmt_t to dictionary structure

--- avrunit.h
+++ avrunit.h
@@ -9,56 +9,46 @@
 #define AU_F_FAIL 1
 #define AU_F_BROKEN 2
 #define AU_F_IGNORE 3
+#define AU_F_SIZE 4
 
 typedef struct {
-    uint8_t fail;
-    uint8_t failID[AU_MAX_TEST];
-    uint8_t broken;
-    uint8_t brokenID[AU_MAX_TEST];
-    uint8_t ignore;
-    uint8_t ignoreID[AU_MAX_TEST];
-    uint8_t run;
+    uint8_t index;
+    uint8_t id[AU_MAX_TEST];
 } stat_t;
 
-stat_t AU_STAT = {0};
-uint8_t AU_STAT_FMT[] = {
-    7,              // Number of record
-    1, AU_MAX_TEST,
-    1, AU_MAX_TEST,
-    1, AU_MAX_TEST,
-    1
+stat_t AU_STAT[AU_F_SIZE];
+
+typedef struct {
+    char label[2];
+} stat_fmt_t;
+
+stat_fmt_t AU_STAT_FMT[AU_F_SIZE] = {
+    {"P", "ID"},
+    {"F", "ID"},
+    {"B", "ID"},
+    {"I", "ID"}
 };
 
 #define AU_OUTPUT_SETUP()
 #define AU_OUTPUT() do { \
-    eeprom_write_block(&AU_STAT_FMT, 0, sizeof(AU_STAT_FMT)); \
-    eeprom_write_block(&AU_STAT, sizeof(AU_STAT_FMT), sizeof(AU_STAT)); \
+    int addr = 0; \
+    eeprom_write_block(&AU_STAT, addr, sizeof(AU_STAT)); \
+    addr = sizeof(AU_STAT); \
+    eeprom_write_block(&AU_STAT_FMT, addr, sizeof(AU_STAT_FMT)); \
 } while (0)
-#define AU_ASSERT(test) do { if (!(test)) { return AU_F_FAIL; } } while (0)
-#define AU_BROKEN(test) do { return AU_F_BROKEN; } while (0)
-#define AU_IGNORED(test) do { return AU_F_IGNORE; } while (0)
+#define AU_ASSERT(TEST) do { if (!(TEST)) { return AU_F_FAIL; } } while (0)
+#define AU_BROKEN(TEST) do { return AU_F_BROKEN; } while (0)
+#define AU_IGNORED(TEST) do { return AU_F_IGNORE; } while (0)
 
 #define AU_UNIT_START
 #define AU_UNIT_END do { return AU_F_PASS; } while(0)
 
-#define AU_RUN_TEST(id, test, ...) do { \
-    int result; \
-    result = test(__VA_ARGS__); \
-    AU_STAT.run++; \
-    switch (result) { \
-        case AU_F_FAIL: \
-            AU_STAT.failID[AU_STAT.fail] = id; \
-            AU_STAT.fail += 1; \
-            break; \
-        case AU_F_BROKEN: \
-            AU_STAT.brokenID[AU_STAT.broken] = id; \
-            AU_STAT.broken += 1; \
-            break; \
-        case AU_F_IGNORE: \
-            AU_STAT.ignoreID[AU_STAT.ignore] = id; \
-            AU_STAT.ignore += 1; \
-            break; \
-    } \
+#define AU_RUN_TEST(ID, TEST_CASE, ...) do { \
+    int flag, index; \
+    flag = TEST_CASE(__VA_ARGS__); \
+    index = AU_STAT[flag].index; \
+    AU_STAT[flag].id[index] = ID; \
+    AU_STAT[flag].index = index + 1; \
 } while (0)
 
 #endif // _AVRUNIT_H
