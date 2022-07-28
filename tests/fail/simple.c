bc4cc9fe511fff3e3221944055030f6011c2c1a2
Khairulmizam Samsudin <xource@gmail.com>
2022-07-28 21:38:14 +0800

    Add test for fail flag

---
+++ tests/fail/simple.c
@@ -0,0 +1,18 @@
+#include "../../avrunit.h"
+
+static int test_fail(void) {
+    AU_UNIT_START;
+
+    AU_ASSERT(1 == 0);
+
+    AU_UNIT_END;
+}
+
+int main (void) {
+
+    AU_RUN_TEST(0x0c, test_fail);
+
+    AU_OUTPUT();
+
+    return (0);
+}
